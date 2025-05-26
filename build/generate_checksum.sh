04e97c93965340bdaea68b26fe07eb7bbb6e4665
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 22:43:49 +0300

    Update generate_checksum.sh

--- build/generate_checksum.sh
+++ build/generate_checksum.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# release/ dizinindeki tüm dosyalar için checksum üretir
 
 for file in release/*; do
   if [[ $file != *.sha256 ]]; then
-    echo "Generating checksum for $file"
-    sha256sum "$file" > "$file.sha256"
+    echo "Generating SHA256 for $(basename $file)"
+    shasum -a 256 "$file" > "$file.sha256"
   fi
 done
