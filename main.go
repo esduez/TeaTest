6ef31d17c43ec211d3c7bf098de690b1dfec2738
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 22:47:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,22 +3,11 @@ package main
 import (
 	"os"
 	"teatest/internal/commands"
-	
-	"github.com/spf13/cobra"
 )
 
 func main() {
-	rootCmd := &cobra.Command{
-		Use:   "teatest",
-		Short: "TeaTest Paket Yöneticisi",
-	}
-
-	rootCmd.AddCommand(commands.NewInstallCmd())
-	rootCmd.AddCommand(commands.NewListCmd())
-	rootCmd.AddCommand(commands.NewSchedulerCmd())
-	rootCmd.AddCommand(commands.NewHashCmd())
-
-	if err := rootCmd.Execute(); err != nil {
+	cmd := commands.NewRootCmd()
+	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
