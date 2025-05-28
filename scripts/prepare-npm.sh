00a748d9dce9a819f7f5229709b7f509e681510d
esduez <60666283+esduez@users.noreply.github.com>
2025-05-28 21:11:53 +0300

    Delete scripts directory

--- scripts/prepare-npm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Go binary'lerini npm için paketler
-for PLATFORM in linux darwin windows; do
-  GOOS=$PLATFORM GOARCH=amd64 go build -o bin/teatest-$PLATFORM
-  tar czf teatest-$PLATFORM.tar.gz -C bin teatest-$PLATFORM
-done
