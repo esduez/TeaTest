31893339995be0f0a6c1ed2f8c81fe0ca605b24e
esduez <60666283+esduez@users.noreply.github.com>
2025-05-28 21:11:30 +0300

    Delete build directory

--- build/generate_checksum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# release/ dizinindeki tüm dosyalar için checksum üretir
-
-for file in release/*; do
-  if [[ $file != *.sha256 ]]; then
-    echo "Generating SHA256 for $(basename $file)"
-    shasum -a 256 "$file" > "$file.sha256"
-  fi
-done
